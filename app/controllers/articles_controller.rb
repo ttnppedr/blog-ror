2e37ed31904921ccd1c15cb7b6e1e4fcdff9f1d8
ttnppedr <priest0204@gmail.com>
2021-08-25 14:48:48 +0800

    Add basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "acc", password: "pass", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
